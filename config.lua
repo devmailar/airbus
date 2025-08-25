646e1a1050d19dfeadcd2d963fcbae9dfe0a0b2f
Harv <86970698+Hxrv3y@users.noreply.github.com>
2025-08-25 19:21:43 +0100

    Create config file

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.CallMarker = vector3(-1038.834, -2733.346, 19.169627)
+Config.Driver = `mp_m_freemode_01`
+Config.Vehicle = `airbus`
+
+Config.MarkerDrawDistance = 15.0
+Config.MarkerInteraction = 1.3
+
+Config.CallMaxDistance = 20.0
+
+return Config
